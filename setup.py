9ac73baf9696ed16f49ce841b69b4f9f73c1c518
Theo Turner <theo_turner@outlook.com>
2019-05-15 13:45:52 +0100

    Clean up package config

--- setup.py
+++ setup.py
@@ -4,10 +4,20 @@ setup(
     name='dlm',
     version='0.0.1',
     description='Data Liquidity Module: Web 3.0 data liquidity with Fetch.AI and Ocean Protocol.',
-    license='MIT',
+    license='Apache License 2.0',
     packages=['dlm'],
+    install_requires=['oef', 'fetchai-ledger-api', 'squid-py'],
     author='Theo Turner',
     author_email='theo@outlierventures.io',
     keywords=['blockchain', 'web3', 'crypto', 'cryptocurrency', 'data', 'liquidity', 'fetchai', 'ocean'],
-    url='https://github.com/OutlierVentures/Data-Liquidity-Module'
+    url='https://github.com/OutlierVentures/Data-Liquidity-Module',
+    classifiers=[
+        'Development Status :: 1 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
 )
