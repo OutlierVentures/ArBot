d8e3c123ebef3421fe07d758e7ce6c79f83c893b
Theo Turner <theo_turner@outlook.com>
2019-05-15 13:05:17 +0100

    Add package config

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dlm',
+    version='0.0.1',
+    description='Data Liquidity Module: Web 3.0 data liquidity with Fetch.AI and Ocean Protocol.',
+    license='MIT',
+    packages=['dlm'],
+    author='Theo Turner',
+    author_email='theo@outlierventures.io',
+    keywords=['blockchain', 'web3', 'crypto', 'cryptocurrency', 'data', 'liquidity', 'fetchai', 'ocean'],
+    url='https://github.com/OutlierVentures/Data-Liquidity-Module'
+)
