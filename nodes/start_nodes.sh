fe0e8c0b26b9c5ac32996d198187d248a6b9b613
Theo Turner <theo_turner@outlook.com>
2019-05-16 16:41:05 +0100

    Add node starter

---
+++ nodes/start_nodes.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+onred='\033[41m'
+ongreen='\033[42m'
+onyellow='\033[43m'
+endcolor="\033[0m"
+
+# Handle errors
+set -e
+error_report() {
+    echo -e "${onred}Error: start_nodes.sh failed on line $1.$endcolor"
+}
+trap 'error_report $LINENO' ERR
+
+if [ ! -z "$DLMNET" ] && [ $DLMNET==TESTNET ]; then
+    ocean=nile
+    echo -e "${onyellow}Starting nodes on testnet...$endcolor"
+else
+    ocean=spree
+    echo -e "${onyellow}Starting nodes for local testing...$endcolor"
+fi
+
+cd fetch_node
+#./oef-core-pluto-image/scripts/docker-run.sh -p 3333:3333 -d --
+
+cd ../ocean_node
+./start_ocean.sh --latest --no-pleuston --no-brizo --local-$ocean-node --force-pull &> /dev/null &
+
+while [ $id!="" ]; do
+    id=$(docker container ls | grep ocean_ | awk '{print $1}')
+    sleep 5
+done
+
+echo -e "${ongreen}Nodes started.$endcolor"
