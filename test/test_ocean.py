4b899fd07bcd06408d9beddc2c4799cfca716f73
Theo Turner <theo_turner@outlook.com>
2019-05-23 14:00:09 +0100

    Extend ocean tests

--- test/test_ocean.py
+++ test/test_ocean.py
@@ -1,13 +1,25 @@
 from dlm.ocean import OceanAgent
-import os
+from dlm.utils import Utils
 
 # Pytest is called from the root directory, so the path to config is from there
 oa = OceanAgent('./dlm/config.ini')
+ut = Utils
+test_time = ut.get_time()
 
 def test_get_account():
     assert len(oa.get_account().__dict__) == 2
     assert len(oa.get_account().address) == 42
 
+def test_publish():
+    created_ddo = oa.publish('Iris Dataset' + test_time,
+                             'Multivariate Iris flower dataset for linear discriminant analysis.',
+                             0,
+                             'https://datahub.io/machine-learning/iris/r/iris.csv',
+                             'CCO: Public Domain',
+                             ['flowers', 'classification', 'plants'])
+    registered_ddo = oa.assets.resolve(created_ddo.did)
+    assert created_ddo.did == registered_ddo.did
+
 def test_search():
-    random = os.urandom(32)
-    assert oa.search(random) == []
+    assert oa.search(test_time) != []
+    assert oa.search('iris') != []
