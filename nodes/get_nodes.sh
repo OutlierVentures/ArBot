fac6a854d9915162a8853bc10f1f1108dc16e38d
Theo Turner <theo_turner@outlook.com>
2019-05-28 17:54:19 +0100

    Update node getter

--- nodes/get_nodes.sh
+++ nodes/get_nodes.sh
@@ -3,7 +3,7 @@
 onred='\033[41m'
 ongreen='\033[42m'
 onyellow='\033[43m'
-endcolor="\033[0m"
+endcolor='\033[0m'
 
 # Handle errors
 set -e
@@ -27,12 +27,15 @@ get_latest() {
 # Install requirements
 echo -e "${onyellow}Installing Ocean Node...$endcolor"
 get_latest oceanprotocol barge
-mv barge ocean_node
 
 echo -e "${onyellow}Installing Fetch Node...$endcolor"
-get_latest fetchai oef-core 
-mv oef-core fetch_node
-cd fetch_node
-./oef-core-pluto-image/scripts/docker-build-img.sh
+if [ ! -d oef-core ]; then
+    git clone https://github.com/fetchai/oef-core
+    cd oef-core
+    git checkout 146c833 # Pre-Pluto
+    ./oef-core-image/scripts/docker-build-img.sh
+else
+    echo "Already up to date."
+fi
 
 echo -e "${ongreen}Nodes installed.$endcolor"
