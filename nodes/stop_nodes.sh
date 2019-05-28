42307ea156b462ce4077bc30b615583da9f8df67
Theo Turner <theo_turner@outlook.com>
2019-05-28 17:55:00 +0100

    Grab nodes error handling if don't exist

--- nodes/stop_nodes.sh
+++ nodes/stop_nodes.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-onred='\033[41m'
 ongreen='\033[42m'
 onyellow='\033[43m'
-endcolor="\033[0m"
-
-# Handle errors
-set -e
-error_report() {
-    echo -e "${onred}Error: stop_nodes.sh failed on line $1.$endcolor"
-}
-trap 'error_report $LINENO' ERR
+endcolor='\033[0m'
 
 echo -e "${onyellow}Stopping nodes...$endcolor"
 
-#docker stop $(docker ps | grep oef-core-pluto-image | awk '{ print $1 }')
+docker stop $(docker ps | grep oef-core-image | awk '{ print $1 }')
 
 docker stop $(docker ps | grep ocean_ | awk '{ print $1 }')
 
