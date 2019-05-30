be4a1da7751c8cfde8d03788e0c654379ec0825d
Theo Turner <theo_turner@outlook.com>
2019-05-30 18:00:44 +0100

    Update ocean tests

--- test/test_ocean.py
+++ test/test_ocean.py
@@ -12,7 +12,7 @@ def test_get_account():
     assert len(oa.get_account().address) == 42
 
 def test_publish():
-    created_ddo = oa.publish('Iris Dataset' + test_time,
+    created_ddo = oa.publish('Iris Dataset ' + test_time,
                              'Multivariate Iris flower dataset for linear discriminant analysis.',
                              0,
                              'https://datahub.io/machine-learning/iris/r/iris.csv',
@@ -23,6 +23,7 @@ def test_publish():
 
 def test_search():
     assert oa.search(test_time) != []
+    print(oa.search(test_time)[0]._services[0].__dict__, oa.search(test_time)[0]._services[1].__dict__, oa.search(test_time)[0]._services[2].__dict__,)
     assert oa.search('iris') != []
 
 @pytest.mark.skip(reason = "Service Agreement type mismatch in Squid-Py")
@@ -31,3 +32,10 @@ def test_consume():
     #oa.accounts.request_tokens(account, 10)
     path_to_data, _ = oa.consume(ddo)
     assert os.path.exists(path_to_data)
+
+def test_get_meta_from_ddo():
+    ddo = oa.search('iris')[0]
+    meta = oa.get_meta_from_ddo(ddo)
+    assert meta['base']['description'] == 'Multivariate Iris flower dataset for linear discriminant analysis.'
+    assert len(meta['base']['tags']) == 3
+
