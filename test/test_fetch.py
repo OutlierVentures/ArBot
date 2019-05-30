477d8dd1a76753d80457630412853c3405fc35f8
Theo Turner <theo_turner@outlook.com>
2019-05-30 18:00:59 +0100

    Add load service to fetch tests

--- test/test_fetch.py
+++ test/test_fetch.py
@@ -1,3 +1,23 @@
 from dlm.fetch import FetchAgent
+from oef.schema import Description
+import pytest
 
-fa = FetchAgent()
+fa = FetchAgent('TestAgent', '127.0.0.1', 3333)
+
+def test_load_service():
+    meta = {
+        'base': {
+            'name': 'Iris Dataset',
+            'description': 'Multivariate Iris flower dataset for linear discriminant analysis.',
+            'tags': [
+                'flowers',
+                'classification',
+                'plants'
+            ]
+        }
+    }
+    service, data = fa.load_service(meta, './test/data/iris_meta.json')
+    assert isinstance(service, Description)
+    assert isinstance(data, dict)
+    with pytest.raises(KeyError):
+        fa.load_service({'not': 'valid'}, '/')
