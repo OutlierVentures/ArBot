db31dc2b26e6db9375f1001bbf55b691a58adb93
Theo Turner <theo_turner@outlook.com>
2019-06-03 17:42:43 +0100

    Add test publish

--- test/test_fetch.py
+++ test/test_fetch.py
@@ -4,20 +4,28 @@ import pytest
 
 fa = FetchAgent('TestAgent', '127.0.0.1', 3333)
 
-def test_load_service():
-    meta = {
-        'base': {
-            'name': 'Iris Dataset',
-            'description': 'Multivariate Iris flower dataset for linear discriminant analysis.',
-            'tags': [
-                'flowers',
-                'classification',
-                'plants'
-            ]
-        }
+meta = {
+    'base': {
+        'name': 'Iris Dataset',
+        'description': 'Multivariate Iris flower dataset for linear discriminant analysis.',
+        'tags': [
+            'flowers',
+            'classification',
+            'plants'
+        ]
     }
-    service, data = fa.load_service(meta, './test/data/iris_meta.json')
+}
+data_path = './test/data/iris_meta.json'
+
+def test_load_service():
+    service, data = fa.load_service(meta, data_path)
     assert isinstance(service, Description)
     assert isinstance(data, dict)
     with pytest.raises(KeyError):
         fa.load_service({'not': 'valid'}, '/')
+
+def test_publish():
+    service, _ = fa.load_service(meta, data_path)
+    fa.connect()
+    fa.publish(service)
+    fa.disconnect()
