7bf6bf9e6bbe832dcbfd527db46c1b04c2898727
Theo Turner <theo_turner@outlook.com>
2019-06-07 16:54:11 +0100

    Higher-order magic

--- test/test_fetch.py
+++ test/test_fetch.py
@@ -19,6 +19,14 @@ mock_counterparty = 'alice'
 
 fa = FetchAgent('TestAgent', '127.0.0.1', 3333, meta, data_path, 0)
 
+# Higher-order helper for functions that need to be online
+def online(function):
+    def connect_work_disconnect():
+        fa.connect()
+        function()
+        fa.disconnect()
+    return connect_work_disconnect
+
 def test_load_service():
     service, data = fa.load_service(meta, data_path)
     assert isinstance(service, Description)
@@ -26,17 +34,16 @@ def test_load_service():
     with pytest.raises(KeyError):
         fa.load_service({'not': 'valid'}, '/')
 
+@online
 def test_publish():
     service, _ = fa.load_service(meta, data_path)
-    fa.connect()
     fa.publish(service)
-    fa.disconnect()
 
+@online
 def test_on_cfp():
-    fa.connect()
     fa.on_cfp(0, 0, mock_counterparty, 0)
-    fa.disconnect()
 
+@online
 def test_on_accept():
     fa.connect()
     fa.on_accept(2, 0, mock_counterparty, 2)
@@ -47,6 +54,7 @@ def test_on_decline():
 
 def test_on_decline():
     fa.on_decline(2, 0, mock_counterparty, 2)
+
 
 
     
