eef1b52d9141e8dae760ef77aeafc4fdf5f4884b
Theo Turner <theo_turner@outlook.com>
2019-06-10 16:19:29 +0100

    Add publishing to o2f

--- demos/o2f.py
+++ demos/o2f.py
@@ -4,3 +4,15 @@ This demo shows an example flow of data liquidity: Ocean to Fetch.
 
 from dlm.ocean import OceanAgent
 from dlm.fetch import FetchAgent
+
+
+oa = OceanAgent('../dlm/config.ini')
+
+# Put the dataset on Ocean to start with. Skip this step and change search term below for existing sets.
+oa.publish(name = 'Iris Dataset',
+           description = 'Multivariate Iris flower dataset for linear discriminant analysis.',
+           price = 0,
+           url = 'https://datahub.io/machine-learning/iris/r/iris.csv',
+           license = 'CC0: Public Domain',
+           tags = ['flowers', 'classification', 'plants'])
+
