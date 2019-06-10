0ece9526a64a4e3f8b0f7894662c88674395f5ed
Theo Turner <theo_turner@outlook.com>
2019-06-10 17:19:54 +0100

    f2o: spin up a plain FetchAgent

--- demos/f2o.py
+++ demos/f2o.py
@@ -1,6 +1,11 @@
 '''
 This demo shows an example flow of data liquidity: Fetch to Ocean.
+Spin the Ocean to Fetch demo up then run this one for a full flow Ocean -> Fetch -> Ocean.
 '''
 
 from dlm.fetch import FetchAgent
 from dlm.ocean import OceanAgent
+
+# Spin up a plain FetchAgent (not holding a dataset).
+fa = FetchAgent('Consumer', oef_addr = '127.0.0.1', oef_port = 3333)
+
