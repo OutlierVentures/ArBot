ffd6adf7632b54a7874b9228f2ee8a1007c73b03
Theo Turner <theo_turner@outlook.com>
2019-06-13 13:16:54 +0100

    Rework o2f demo

--- demo/o2f.py
+++ demo/o2f.py
@@ -6,37 +6,29 @@ Spin this up, then spin the Fetch to Ocean demo up for a full flow Ocean -> Fetc
 from dlm.ocean import OceanAgent
 from dlm.fetch import FetchAgent
 
-
 oa = OceanAgent('../dlm/config.ini')
 
 # Put the dataset on Ocean to start with. Skip this step and change search term below for existing sets.
-oa.publish(name = 'Iris Dataset',
-           description = 'Multivariate Iris flower dataset for linear discriminant analysis.',
-           price = 0,
-           url = 'https://pkgstore.datahub.io/machine-learning/iris/iris_json/data/23a7b3de91da915b506f7ca23f6d1141/iris_json.json',
-           license = 'CC0: Public Domain',
-           tags = ['flowers', 'classification', 'plants', 'outlier ventures'])
-
-# Search for desired sets.
-list_of_ddos = oa.search('outlier ventures')
+oa.publish('Iris Dataset',
+           'Multivariate Iris flower dataset for linear discriminant analysis.',
+           0,
+           'https://pkgstore.datahub.io/machine-learning/iris/iris_json/data/23a7b3de91da915b506f7ca23f6d1141/iris_json.json',
+           'CCO: Public Domain',
+           ['flowers', 'classification', 'plants'])
+list_of_ddos = oa.search('flowers')
 ddo = list_of_ddos[0]
-
 # Consume found set. Mock data consumption while squid-py issue #382 is open.
 #path_to_data, _ = oa.consume(ddo)
 path_to_data = '../test/data/iris.json'
 
-# Spin up a Fetch agent with our dataset.
 fa = FetchAgent(public_key = 'Provider',
                 oef_addr = '127.0.0.1',
                 oef_port = 3333,
-                path_to_data = path_to_data,
+                load_path = path_to_data,
                 metadata = oa.get_meta_from_ddo(ddo))
 fa.connect()
-
-# Publish to Fetch's OEF.
-fa.publish(fa.service)
+fa.publish()
 print('Published to the OEF.')
-
 try:
     fa.run()
 finally:
