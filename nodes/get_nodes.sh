d3d2c1f3831d97831cb9a763ee6bad37d04b65b7
Theo Turner <theo_turner@outlook.com>
2019-08-02 14:00:17 +0100

    Node getter now fetches deps

--- nodes/get_nodes.sh
+++ nodes/get_nodes.sh
@@ -24,6 +24,28 @@ get_latest() {
     cd ..
 }
 
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    sudo apt-get update
+    sudo apt-get install -y build-essential python3
+    bazel version || echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list && \
+                     curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add - && \
+                     sudo apt-get update && \
+                     sudo apt-get install bazel
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    xcode-select --version || xcode-select --install
+    brew --version || yes | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    python3 --version || brew install python
+    if brew ls --versions bazel >/dev/null; then
+        if [[ $(brew outdated bazel) ]]; then
+            HOMEBREW_NO_AUTO_UPDATE=1 brew upgrade bazelbuild/tap/bazel
+        fi
+    else
+        brew tap bazelbuild/tap
+        HOMEBREW_NO_AUTO_UPDATE=1 brew install bazelbuild/tap/bazel
+    fi
+fi
+
 # Install requirements
 echo -e "${onyellow}Installing Ocean Node...$endcolor"
 get_latest oceanprotocol barge
