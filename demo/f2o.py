ceb65f36287a58addc728a1f671e39bd4c58bc3b
Theo Turner <theo_turner@outlook.com>
2019-08-06 19:22:21 +0100

    F2O demo with interactable FIPA proposals

--- demo/f2o.py
+++ demo/f2o.py
@@ -8,7 +8,9 @@ from dlm.ocean import OceanAgent
 
 fa = FetchAgent()
 fa.connect()
-fa.search('flowers', 0, './purchased.json')
+fa.fetch_search('flowers')
+#search_results = fa.fetch_get_search_results()
+fa.fetch_consume(number_to_consume = 1, save_path = './')
 try:
     fa.run()
 finally:
@@ -16,9 +18,9 @@ finally:
     fa.disconnect()
 
 oa = OceanAgent('../dlm/config.ini')
-oa.ocean_publish('Iris Dataset',
-                 'Multivariate Iris flower dataset for linear discriminant analysis.',
-                 0,
-                 'https://pkgstore.datahub.io/machine-learning/iris/iris_json/data/23a7b3de91da915b506f7ca23f6d1141/iris_json.json',
-                 'CCO: Public Domain',
-                 ['flowers', 'classification', 'plants'])
+oa.ocean_publish(name = 'Iris Dataset',
+                 description = 'Multivariate Iris flower dataset for linear discriminant analysis.',
+                 price = 0,
+                 url = 'https://pkgstore.datahub.io/machine-learning/iris/iris_json/data/23a7b3de91da915b506f7ca23f6d1141/iris_json.json',
+                 license = 'CCO: Public Domain',
+                 tags = ['flowers', 'classification', 'plants'])
