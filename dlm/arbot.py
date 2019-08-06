f3f8e675d38ca8b4b3a0eb4e50add1fa35cc2ab2
Theo Turner <theo_turner@outlook.com>
2019-08-06 19:21:51 +0100

    Arbot wireframes + tests

--- dlm/arbot.py
+++ dlm/arbot.py
@@ -6,10 +6,18 @@ class ArBot(FetchAgent, OceanAgent):
 
     def __init__(self, path_to_config):
        #OceanAgent.__init__(self, path_to_config)
+       FetchAgent.__init__(self)
        pass
 
     def arb_terms(self, terms):
         return True
+
+    def arb_f2o(self, terms, price_limit):
+        #ocean_demand = self.ocean_search(tersm)
+        #self.fetch_search(terms)
+        #fetch_supply = self.fetch_get_search_results()
+        return True
+
 
 
 if __name__ == '__main__':
--- test/test_arbot.py
+++ test/test_arbot.py
@@ -2,6 +2,8 @@ from dlm.arbot import ArBot
 
 ab = ArBot('./dlm/config.ini')
 
-def test_arb_word():
+def test_arb_terms():
     assert ab.arb_terms(['iris'])
 
+def test_arb_f20():
+    assert ab.arb_f2o(terms = ['iris'], price_limit = 100)
