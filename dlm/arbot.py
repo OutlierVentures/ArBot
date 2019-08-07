0eb219f43592adb38b9aa6f795795e7af70a294e
Theo Turner <theo_turner@outlook.com>
2019-08-07 12:39:54 +0100

    Add get usd value

--- dlm/arbot.py
+++ dlm/arbot.py
@@ -1,12 +1,14 @@
 from dlm.fetch import FetchAgent
 from dlm.ocean import OceanAgent
+from pycoingecko import CoinGeckoAPI
 
 
-class ArBot(FetchAgent, OceanAgent):
+class ArBot(FetchAgent, OceanAgent, CoinGeckoAPI):
 
     def __init__(self, path_to_config):
        #OceanAgent.__init__(self, path_to_config)
        FetchAgent.__init__(self)
+       CoinGeckoAPI.__init__(self)
        pass
 
     def arb_terms(self, terms):
@@ -17,6 +19,10 @@ class ArBot(FetchAgent, OceanAgent):
         #self.fetch_search(terms)
         #fetch_supply = self.fetch_get_search_results()
         return True
+    
+    def get_usd_value(self, name):
+        price_dict = self.get_price(ids = name, vs_currencies = 'usd')
+        return price_dict[name]['usd']
 
 
 
