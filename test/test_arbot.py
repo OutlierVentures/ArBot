1852bbdb8a5590ff627df26e279afcedb29538bf
Theo Turner <theo_turner@outlook.com>
2019-08-07 19:17:34 +0100

    Add arbot arb test

--- test/test_arbot.py
+++ test/test_arbot.py
@@ -1,15 +1,50 @@
 from dlm.arbot import ArBot
-import pytest
+from dlm.fetch import FetchAgent
+from dlm.ocean import OceanAgent
+import pytest, threading, time, os
 
-ab = ArBot('./dlm/config.ini')
+meta = {
+    'base': {
+        'name': 'Iris Dataset',
+        'description': 'Multivariate Iris flower dataset for linear discriminant analysis.',
+        'tags': [
+            'flowers',
+            'classification',
+            'plants'
+        ]
+    }
+}
+
+ab = ArBot('./dlm/config.ini', './test/data/', 'file://' + os.getcwd())
 
 def test_get_usd_value():
     assert isinstance(ab.get_usd_value('fetch-ai'), float)
     with pytest.raises(KeyError):
         ab.get_usd_value('fetchai')
 
-def test_arb_terms():
-    assert ab.arb_terms(['iris'])
+def test_arb():   
+    thread_one = threading.Thread(target = run_fetch_agent())
+    thread_two = threading.Thread(target = run_ocean_agent())
+    thread_one.start()
+    thread_two.start()
+    time.sleep(2)
+    assert ab.arb('iris classification')
 
-def test_arb_f20():
-    assert ab.arb_f2o(terms = ['iris'], price_limit = 100)
+def run_fetch_agent():
+    fa = FetchAgent()
+    fa.connect()
+    fa.fetch_publish_from_ocean_meta(meta, 0, './data/iris.json')
+    try:
+        fa.run()
+    finally:
+        fa.stop()
+        fa.disconnect()
+
+def run_ocean_agent():
+    oa = OceanAgent('./dlm/config.ini')
+    oa.ocean_publish(name = meta['base']['name'],
+                     description = meta['base']['description'],
+                     price = 100,
+                     url = 'https://pkgstore.datahub.io/machine-learning/iris/iris_json/data/23a7b3de91da915b506f7ca23f6d1141/iris_json.json',
+                     license = 'CCO: Public Domain',
+                     tags = meta['base']['tags'])
