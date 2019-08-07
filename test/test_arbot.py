19e6917edcc0ce24863f2bf2e2b019ede25cdb97
Theo Turner <theo_turner@outlook.com>
2019-08-07 12:39:47 +0100

    Add test get USD value

--- test/test_arbot.py
+++ test/test_arbot.py
@@ -1,6 +1,12 @@
 from dlm.arbot import ArBot
+import pytest
 
 ab = ArBot('./dlm/config.ini')
+
+def test_get_usd_value():
+    assert isinstance(ab.get_usd_value('fetch-ai'), float)
+    with pytest.raises(KeyError):
+        ab.get_usd_value('fetchai')
 
 def test_arb_terms():
     assert ab.arb_terms(['iris'])
